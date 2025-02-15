692b4c4b7a3808df8339cd2ff8c19e70dc4e9288
SahanaUJ <sahanajogur72@gmail.com>
2025-02-15 16:10:15 +0530

    modifying user - added new field

--- src/main/java/com/example/git/model/User.java
+++ src/main/java/com/example/git/model/User.java
@@ -3,7 +3,16 @@ package com.example.git.model;
 public class User {
     private int id;
     private String username;
+    private String email;
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public int getId() {
         return id;
